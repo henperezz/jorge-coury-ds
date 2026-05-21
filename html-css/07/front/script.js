a8cedffe6ce481b6803e701acec826de31cf9405
Pedro Henrique Perez <pedro.henrique.perez08@gmail.com>
2026-05-21 08:07:59 -0300

    delete unused .html, and update script.js, style.css and index.html with functional switch between screens

--- html-css/07/front/script.js
+++ html-css/07/front/script.js
@@ -1,12 +1,17 @@
-let signUpLink = document.querySelector(".signUpLink");
+let signUpLink = document.getElementById("signUpLink");
 let cadastro = document.querySelector(".buttonCadastro");
 
-signUpLink.addEventListener("click", () => {
+let menuLogin = document.querySelector(".menuLogin");
+let menuSignUp = document.querySelector(".menuSignUp");
+
+signUpLink.addEventListener("click", (e) => {
+  e.preventDefault;
   menuLogin.classList.remove("activated");
   menuSignUp.classList.add("activated");
 });
 
-cadastro.addEventListener("click", () => {
+cadastro.addEventListener("click", (e) => {
+  e.preventDefault;
   menuSignUp.classList.remove("activated");
   menuLogin.classList.add("activated");
 });
