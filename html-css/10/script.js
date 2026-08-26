260d992425e2116242f717e2d13277f03b6430ce
henperezz <pedro.henrique.perez08@gmail.com>
2026-08-25 21:57:05 -0300

    update tempoClima (10) project

--- html-css/10/script.js
+++ html-css/10/script.js
@@ -3,24 +3,38 @@ const botaoBuscar = document.getElementById("botao-busca");
 botaoBuscar.addEventListener("click", buscarClima);
 
 function buscarClima() {
-  const cidade = document.getElementById("cidade").ariaValueMax.trim();
+  const cidade = document.getElementById("pesquisa-cidade").value.trim();
 
   if (cidade === "") {
     alert("Pesquise alguma cidade!!!");
+    return;
   }
-  return;
+
+  const chaveApi = "824143e6dc4c899164e3d851f20f4258";
+
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${chaveApi}&lang=pt-br&units=metric`;
+
+  fetch(url)
+    .then(function (resposta) {
+      if (!resposta.ok) {
+        throw new Error("Erro na busca da cidade, tente novamente.");
+      }
+
+      return resposta.json();
+    })
+
+    .then(function (infos) {
+      console.log(infos);
+
+      document.getElementById("nome-cidade").textContent = infos.name;
+
+      document.getElementById("min-temp").textContent =
+        infos.main.temp_min.toFixed(1);
+
+      document.getElementById("temp-atual").textContent =
+        infos.main.temp.toFixed(1);
+
+      document.getElementById("max-temp").textContent =
+        infos.main.temp_max.toFixed(1);
+    });
 }
-const chaveApi = "824143e6dc4c899164e3d851f20f4258";
-
-const url = `https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${chaveApi}&lang=pt-br&units=metric`;
-
-fetch(url)
-  .then(function (resposta) {
-    if (!resposta.ok) {
-      throw new Error("Erro na busca da cidade, tente novamente.");
-    }
-
-    return resposta.json();
-  })
-
-  .then(function (infos) {});
